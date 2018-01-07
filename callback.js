bbe9f06938df61c0c8ec7d309965117f53f89526
Safwan Ahmad <safwan.pico@gmail.com>
2018-01-07 17:02:12 +0530

    Connect plotly graphs with bokeh sliders

--- callback.js
+++ callback.js
@@ -1,34 +1,50 @@
-var f = cb_obj.value;
-    data = source1.data
-    
-    x = data['x']
-    y = data['y']
-    x_min = x[0]
-    x_max = x[1]
-    y_min = y[0]
-    y_max = y[1]
-    
+    // cb_obj is the Bokeh slider whose value was changed
+    var slider_value = cb_obj.value;
     var title = cb_obj.title;
+
+    if (title == "Slope")
+    {
+        new_slope = slider_value
+        new_intecept = intercept
+    }
+    else
+    {
+        new_slope = slope
+        new_intercept = slider_value
+    }
+    
+    xy_extents = xy_extents.data
+    
+    x_extents = xy_extents['x']
+    y_extents = xy_extents['y']
+    x_min = x_extents[0]
+    x_max = x_extents[1]
+    y_min = y_extents[0]
+    y_max = y_extents[1]
+    
+    
     var slope = (y_max - y_min)/(x_max - x_min + 0.00001);
     var intercept = y_min - slope * x_min;
 
     
-    if (title == "Slope")
-    {
-        y[0] = Math.tan(f) * x[0] + intercept;
-        y[1] = Math.tan(f) * x[1] + intercept;
-    }
-    else
-    {
-        y[0] = slope * x[0] + f;
-        y[1] = slope * x[1] + f;
-    }
+
+    c_square = cost_coeffs['c**2']
+    m_square = cost_coeffs['m**2']
+    c_term   = cost_coeffs['c']
+    m_term   = cost_coeffs['m']
+    c_m_term = cost_coeffs['c*m'] 
+    constant = cost_coeffs['1']
     
-    // Update the cost plot which is actually a Plotly
-    // plot through ugly DOM / javascript hacking.
+    var old_cost = (c_square*intercept*intercept) + (m_square*slope*slope)
+    + (c_term*intercept) + (m_term*slope) + (c_m_term*intercept*slope)
+    + constant
     
-    var update = {
-    title: 'some new title'};
+    var new_cost = (c_square*new_intercept*new_intercept) + (m_square*new_slope*new_slope)
+    + (c_term*new_intercept) + (m_term*new_slope) + (c_m_term*new_intercept*new_slope)
+    + constant
+    
+    // Update line and cost plots which are actually Plotly
+    // plots through ugly DOM / javascript manipulation.
     
     var id = document.getElementsByClassName('plotly-graph-div js-plotly-plot')[0].id
     
@@ -39,27 +55,13 @@ var f = cb_obj.value;
     var sphere_data = plotData[1]
     
     
-    cost_coeffs = source2.data['coeffs'];
-    sphere = source3.data['sphere'];
+    cost_coeffs = cost_coeffs.data['coeffs'];
+    x_sphere = sphere_coordinates.data['x'];
+    y_sphere = sphere_coordinates.data['y'];
+    z_sphere = sphere_coordinates.data['z'];
     
-    c_square = cost_coeffs[0]
-    m_square = cost_coeffs[1]
-    c_term   = cost_coeffs[2]
-    m_term   = cost_coeffs[3]
-    c_m_term = cost_coeffs[4] 
-    constant = cost_coeffs[5]
-    
-    x_sphere = sphere[0]
-    y_sphere = sphere[1]
-    z_sphere = sphere[2]
 
-    var old_cost = (c_square*intercept*intercept) + (m_square*slope*slope)
-    + (c_term*intercept) + (m_term*slope) + (c_m_term*intercept*slope)
-    + constant
-    
-    
-    var new_slope     = ( (y[1] - y[0])/(x[1] - x[0] + 0.000001) )
-    var new_intercept = ( y[1] - new_slope * x[0])
+   
     
     var new_x_sphere = new Array(x_sphere.length)
     var new_y_sphere = new Array(y_sphere.length)
@@ -77,9 +79,7 @@ var f = cb_obj.value;
         
     }
     
-    var new_cost = (c_square*new_intercept*new_intercept) + (m_square*new_slope*new_slope)
-    + (c_term*new_intercept) + (m_term*new_slope) + (c_m_term*new_intercept*new_slope)
-    + constant
+
     
     for(i = 0; i < z_sphere.length; i++)
     {
